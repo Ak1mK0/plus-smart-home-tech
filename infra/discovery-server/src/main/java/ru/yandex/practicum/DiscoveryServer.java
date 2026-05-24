56f847eeffb1c6114f67f49de93854c3f46b0fa2
Akim Kolesnik <kolakim@yandex.ru>
2026-05-24 13:21:27 +0300

    fix error in discovery file name

--- infra/discovery-server/src/main/java/ru/yandex/practicum/DiscoveryServer.java
+++ infra/discovery-server/src/main/java/ru/yandex/practicum/DiscoveryServer.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class DiscoverServer {
+public class DiscoveryServer {
     public static void main(String[] args) {
-        SpringApplication.run(DiscoverServer.class, args);
+        SpringApplication.run(DiscoveryServer.class, args);
     }
 }
